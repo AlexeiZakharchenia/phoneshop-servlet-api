3242f296f49ff20ecb577964d28d3902d5f2d927
Alexei Zakharchenia <alexeizakh@mail.ru>
2019-03-17 17:36:57 +0300

    Task 1.5 Implemented ProductDao

--- src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
+++ src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
@@ -1,25 +1,44 @@
 package com.es.phoneshop.model.product;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ArrayListProductDao implements ProductDao {
+
+    private List<Product> products = new ArrayList<>();
+
+
     @Override
-    public Product getProduct(Long id) {
-        throw new RuntimeException("Not implemented");
+    public synchronized Product getProduct(Long id) {
+        return products.stream().filter(p -> p.getId().equals(id) &&
+                p.getPrice() != null &&
+                p.getStock() > 0).findFirst().
+                orElseThrow(() -> new IllegalArgumentException("Product with that id not exists"));
     }
 
     @Override
-    public List<Product> findProducts() {
-        throw new RuntimeException("Not implemented");
+    public synchronized List<Product> findProducts() {
+        return products.stream().filter(p -> p.getPrice() != null &&
+                p.getStock() > 0)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public void save(Product product) {
-        throw new RuntimeException("Not implemented");
+    public synchronized void save(Product product) {
+        if (products.stream()
+                .anyMatch(p -> p.getId().equals(product.getId()))) {
+            throw new IllegalArgumentException("Product with that id already exists");
+        } else {
+            products.add(product);
+        }
     }
 
     @Override
-    public void delete(Long id) {
-        throw new RuntimeException("Not implemented");
+    public synchronized void delete(Long id) {
+        products.stream().filter(p -> (p.getId().equals(id)))
+                .findFirst().map(i -> products.remove(i))
+                .orElseThrow(() -> new IllegalArgumentException("Rroduct woth that id not existing"));
     }
+
 }
--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -2,12 +2,24 @@ package com.es.phoneshop.model.product;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
+import java.math.BigDecimal;
+import java.util.Currency;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class ArrayListProductDaoTest
 {
     private ProductDao productDao;
+
+
+    @Mock
+    private Currency usd;
+
+    @Mock
+    private String imageUrl;
 
     @Before
     public void setup() {
@@ -18,4 +30,21 @@ public class ArrayListProductDaoTest
     public void testFindProductsNoResults() {
         assertTrue(productDao.findProducts().isEmpty());
     }
+
+
+    @Test
+    public void testSaveAndDelete() {
+        Product product = new Product(1L, "sgs", "Samsung Galaxy S", new BigDecimal(100), usd, 100, imageUrl);
+        productDao.save(product);
+        assertEquals(1, productDao.findProducts().size());
+        productDao.delete(1L);
+        assertEquals(0, productDao.findProducts().size());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetProduct() {
+        productDao.getProduct(1L);
+    }
+
+
 }
