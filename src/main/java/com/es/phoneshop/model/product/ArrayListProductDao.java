e8df2193839d8996e7e88a9b996895d846c3e282
Alexei Zakharchenia <alexeizakh@mail.ru>
2019-03-19 17:47:41 +0300

    Task 1.5 Fixed method delete, add method findActualProducts. Task 1.5 Fixed ProductDaoTest.

--- src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
+++ src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
@@ -3,24 +3,29 @@ package com.es.phoneshop.model.product;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ArrayListProductDao implements ProductDao {
 
     private List<Product> products = new ArrayList<>();
 
 
+    private Stream<Product> getActualProducts() {
+        return products.stream().filter(p -> p.getPrice() != null &&
+                p.getStock() > 0);
+    }
+
+
     @Override
     public synchronized Product getProduct(Long id) {
-        return products.stream().filter(p -> p.getId().equals(id) &&
-                p.getPrice() != null &&
-                p.getStock() > 0).findFirst().
-                orElseThrow(() -> new IllegalArgumentException("Product with that id not exists"));
+        return getActualProducts().filter(p -> p.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Product with that id not exists"));
     }
 
     @Override
     public synchronized List<Product> findProducts() {
-        return products.stream().filter(p -> p.getPrice() != null &&
-                p.getStock() > 0)
+        return getActualProducts()
                 .collect(Collectors.toList());
     }
 
@@ -36,9 +41,9 @@ public class ArrayListProductDao implements ProductDao {
 
     @Override
     public synchronized void delete(Long id) {
-        products.stream().filter(p -> (p.getId().equals(id)))
-                .findFirst().map(i -> products.remove(i))
-                .orElseThrow(() -> new IllegalArgumentException("Rroduct woth that id not existing"));
+        if (!products.removeIf(p -> p.getId().equals(id))) {
+            throw new IllegalArgumentException("Product with that id not exists");
+        }
     }
 
 }
--- src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
+++ src/test/java/com/es/phoneshop/model/product/ArrayListProductDaoTest.java
@@ -8,7 +8,6 @@ import java.math.BigDecimal;
 import java.util.Currency;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class ArrayListProductDaoTest
 {
@@ -24,26 +23,35 @@ public class ArrayListProductDaoTest
     @Before
     public void setup() {
         productDao = new ArrayListProductDao();
+        productDao.save(new Product(1L, "sgs", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg"));
+        productDao.save(new Product(2L, "sgs2", "Samsung Galaxy S II", new BigDecimal(200), usd, 0, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S%20II.jpg"));
+        productDao.save(new Product(3L, "sgs3", "Samsung Galaxy S III", new BigDecimal(300), usd, 5, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S%20III.jpg"));
     }
 
     @Test
-    public void testFindProductsNoResults() {
-        assertTrue(productDao.findProducts().isEmpty());
+    public void FindProducts() {
+        assertEquals(2, productDao.findProducts().size());
     }
 
 
     @Test
-    public void testSaveAndDelete() {
-        Product product = new Product(1L, "sgs", "Samsung Galaxy S", new BigDecimal(100), usd, 100, imageUrl);
+    public void testSave() {
+        Product product = new Product(4L, "iphone", "Apple iPhone", new BigDecimal(200), usd, 10, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Apple/Apple%20iPhone.jpg");
         productDao.save(product);
-        assertEquals(1, productDao.findProducts().size());
-        productDao.delete(1L);
-        assertEquals(0, productDao.findProducts().size());
+        assertEquals(3, productDao.findProducts().size());
+
+    }
+
+
+    @Test
+    public void testDelete() {
+        productDao.delete(2L);
+        assertEquals(2, productDao.findProducts().size());
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testGetProduct() {
-        productDao.getProduct(1L);
+        productDao.getProduct(5L);
     }
 
 
